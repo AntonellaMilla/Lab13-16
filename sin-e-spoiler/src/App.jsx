d8376e51c819e8d3e31ce8f7f12aa7e9ee80e5c8
AntonellaMilla <antonella.milla@tecsup.edu.pe>
2025-06-11 17:13:17 -0500

    Add files via upload

---
+++ sin-e-spoiler/src/App.jsx
@@ -0,0 +1,16 @@
+
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+function App() {
+
+  return (
+    <>
+      <Header />
+      <h1>Las mejores peliculas de la historia</h1>
+    </>
+    <Footer />
+    
+  )
+}
+
+export default App
